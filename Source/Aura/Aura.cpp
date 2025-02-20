877997d7482d3a1d742840bae3774971a6fc5972
Skorlyton <167907708+skorlyton@users.noreply.github.com>
2025-02-21 02:59:51 +0700

    first

---
+++ Source/Aura/Aura.cpp
@@ -0,0 +1,6 @@
+// Copyright Hung
+
+#include "Aura.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Aura, "Aura" );
---
+++ Source/Aura/Aura.h
@@ -0,0 +1,6 @@
+// Copyright Hung
+
+#pragma once
+
+#include "CoreMinimal.h"
+
